3ac43fccc0cf5b4ddc83c26e11fdf8f210d40b02
Aydar Asadullin <aiasadullin_2@edu.hse.ru>
2023-07-08 19:48:06 +0300

    added intructions

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ from image2fen import FEN_Converter
 bot_token = 'REMOVED'
 bot = telebot.TeleBot(bot_token)
 model_path = 'model/warmup_normal_final.pt'
-start_mes = "Привет это бот для автоматического распознавания шахматной доски, просто пришли мне фото следуя инструкции и получи представление в электронном виде"
+start_mes = "Привет это бот для автоматического распознавания шахматной доски, просто пришли мне фото следуя инструкции и получи представление в электронном виде на сайте lichess.org"
+instruction_mes = "Присылай изображение строго в таком формате:\n" \
+                  "1) Изображение шахматной доски строго сбоку, при этом белый фигуры должны быть СЛЕВА, черные фигуры СПРАВА\n" \
+                  "2) Доска на фото должна быть расположена ПАРАЛЛЕЛЬНО горизонту \n" \
+                  "3) Фото должно быть КВАДРАТНЫМ, но чтобы в него полностью попадала шахматная доска (обрезать можно с помощью встроенного редактора в telegram)\n"
 detector = FEN_Converter(model_path)
 test = True
 
@@ -12,8 +16,7 @@ test = True
 def start_message(message):
     print(message.chat.id)
     bot.send_message(message.chat.id, start_mes)
-
-
+    bot.send_photo(message.chat.id, open('example.jpg', 'rb'), instruction_mes)
 admin_id = 1893505394
 @bot.message_handler(content_types=['photo'])
 def photo(message):
