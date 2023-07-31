03a65f2fa0e84bf5b597f5c77f34d0646a63f01b
Aydar Asadullin <aiasadullin_2@edu.hse.ru>
2023-07-31 13:34:34 +0300

    added autodownload model from yadisk

--- pieces/piece_detector.py
+++ pieces/piece_detector.py
@@ -1,10 +1,23 @@
 from ultralytics import YOLO
 import cv2
+import yadisk
 
+def download_model(path):
+    print("Started downloading model")
+    # url to get token: REMOVED
+    yandex_disk_token = "REMOVED"
+    disk_path = "Модель шахмат/warmup_normal_final.pt"
+    disk = yadisk.YaDisk(token=yandex_disk_token)
+    disk.download(disk_path, path)
+    print("Download finished")
 
 def load_model(path):
-    return YOLO(path)
-
+    try:
+        return YOLO(path)
+    except:
+        print("Failed to load model")
+        download_model(path)
+        return YOLO(path)
 
 def chess_pieces_detector(image, model):
     results = model.predict(source=image, conf=0.5, augment=False, save=False)
